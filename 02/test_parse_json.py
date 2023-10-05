6cc01a60e38b726e2aedb61f62188699cd0f19f8
Sofia <58223043+pssprch@users.noreply.github.com>
2023-10-05 14:59:57 +0300

    Add files via upload

---
+++ 02/test_parse_json.py
@@ -0,0 +1,44 @@
+import unittest
+from unittest.mock import Mock
+from parse_json import parse_json
+
+class TestParseJson(unittest.TestCase):
+
+    def setUp(self):
+        self.json_str = '{"key1": "Word1 word2", "key2": "word2 word3"}'
+
+    def test_valid(self):
+        func = Mock()
+        parse_json(self.json_str, required_fields=["key1"], keywords=["word2"], keyword_callback=func)
+        func.assert_called_once_with("key1", "word2")
+    
+    def test_invalid(self):
+        invalid_json_str = '{"key1": word1"}'
+        func = Mock()
+        with self.assertLogs() as cm:
+            parse_json(invalid_json_str, keyword_callback=func)
+        self.assertIn("JSON Decode Error", cm.output[0])
+        func.assert_not_called()
+
+    def test_no_kw(self):
+        func = Mock()
+        parse_json(self.json_str, required_fields=["key1"], keywords=["word4"], keyword_callback=func)
+        func.assert_not_called()
+
+    def test_no_req(self):
+        func = Mock()
+        parse_json(self.json_str, keywords=["key1"], keyword_callback=func)
+        func.assert_not_called()
+
+    def test_case(self):
+        func = Mock()
+        parse_json(self.json_str, required_fields=["key2"], keywords=["WORD2"],keyword_callback=func)
+        func.assert_called_once_with("key2", "WORD2")
+
+    def test_empty(self):
+        func = Mock()
+        parse_json(" ", keyword_callback=func)
+        func.assert_not_called()
+
+if __name__ == "__main__":
+    unittest.main()
