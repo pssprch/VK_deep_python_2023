4fafb8c748691af48c8fccd89dab31edd8bb0021
Sofia <58223043+pssprch@users.noreply.github.com>
2023-10-18 00:06:31 +0300

    Update parse_json.py

--- 02/parse_json.py
+++ 02/parse_json.py
@@ -3,26 +3,32 @@ import json
 
 logger = logging.getLogger(__name__)
 
+
 def parse_json(json_str: str, required_fields=None, keywords=None, keyword_callback=None):
-    
     if required_fields is None:
         required_fields = []
+
     if keywords is None:
         keywords = []
 
+    if keyword_callback is None:
+        def default_keyword_callback(field, keyword):
+            print(f"Keyword '{keyword}' in field '{field}'")
+
+        keyword_callback = default_keyword_callback
+
     try:
-        json_doc  = json.loads(json_str)
+        json_doc = json.loads(json_str)
     except json.JSONDecodeError:
         logger.info("JSON Decode Error")
-        # print("JSON Decode Error")
         return
-    
+
     for field, value in json_doc.items():
         if field in required_fields:
             for keyword in keywords:
-                if keyword.lower() in value.lower():
+                if keyword.lower() in value.lower().split():
                     keyword_callback(field, keyword)
+
 
 def keyword_callback(field, keyword):
     print(f"Keyword '{keyword}' in field '{field}'")
-    
