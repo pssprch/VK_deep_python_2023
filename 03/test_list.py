17f4afc38d5008fc1a4673bc9928da5cecc063ff
Sofia <58223043+pssprch@users.noreply.github.com>
2023-10-18 00:33:45 +0300

    Update test_list.py

--- 03/test_list.py
+++ 03/test_list.py
@@ -14,12 +14,15 @@ class TestCustomList(unittest.TestCase):
         self.assertEqual(cl1 + cl2, CustomList([4, 5, 3, 4, 5]))
         self.assertEqual(cl2 + cl1, CustomList([4, 5, 3, 4, 5]))
         self.assertEqual(cl1 + cl3, CustomList([1, 2, 3, 4, 5]))
-        self.assertEqual([1, 2, 3, 4, 5], CustomList([1, 2, 3, 4, 5]))
         self.assertEqual(cl2 + cl3, CustomList([3, 3]))
         self.assertIsInstance(cl2 + lst, CustomList)
         self.assertIsInstance(lst + cl2, CustomList)
-        self.assertEqual(cl2 + lst, [-4, 4])
+        self.assertEqual(CustomList(lst) + cl2, CustomList([-4, 4]))
+        self.assertEqual(cl2 + lst, CustomList([-4, 4]))
         self.assertEqual(lst + cl2, [-4, 4])
+
+        self.assertEqual(cl1, CustomList([1, 2, 3, 4, 5]))
+        self.assertEqual(cl2, CustomList([3, 3]))
 
     def test_sub(self):
         cl1 = CustomList([1, 2, 3, 4, 5, 6])
@@ -36,32 +39,37 @@ class TestCustomList(unittest.TestCase):
         self.assertIsInstance(cl1 - lst, CustomList)
         self.assertIsInstance(lst - cl1, CustomList)
         self.assertEqual(cl1 - lst, CustomList([4, 1, 1, 4, 5, 6]))
-        self.assertEqual(lst - cl1, CustomList([-4, -1, -1, -4, -5, -6]))
+        self.assertEqual(CustomList(lst) - cl1, CustomList([-4, -1, -1, -4, -5, -6]))
+
+        self.assertEqual(cl1, CustomList([1, 2, 3, 4, 5, 6]))
+        self.assertEqual(cl2, CustomList([2, 5, -7]))
 
     def test_comp(self):
         cl1 = CustomList([1, 2, 3, 4])
         cl2 = CustomList([1, 2, 3, 4])
+        cl3 = CustomList([2, 3, 4, 5])
 
         self.assertEqual(cl1, cl2)
+        self.assertNotEqual(cl1, cl3)
         self.assertLessEqual(cl1, cl2)
         self.assertGreaterEqual(cl1, cl2)
 
-        cl1 = CustomList([1, 2, 3, 4, -5])
-        cl2 = CustomList([1, 2, 3, 5, -6])
+        cl4 = CustomList([1, 2, 3, 4, -5])
+        cl5 = CustomList([1, 2, 3, 5, -6])
 
-        self.assertEqual(cl1, cl2)
-        self.assertLessEqual(cl1, cl2)
-        self.assertGreaterEqual(cl1, cl2)
+        self.assertEqual(cl4, cl5)
+        self.assertLessEqual(cl4, cl5)
+        self.assertGreaterEqual(cl4, cl5)
 
-        cl1 = CustomList([-6, -7, -8])
-        cl2 = CustomList([1])
+        cl6 = CustomList([-6, -7, -8])
+        cl7 = CustomList([1])
 
-        self.assertLess(cl1, cl2)
-        self.assertLessEqual(cl1, cl2)
-        self.assertNotEqual(cl1, cl2)
-        self.assertGreater(cl2, cl1)
-        self.assertGreaterEqual(cl2, cl1)
-        self.assertNotEqual(cl2, cl1)
+        self.assertLess(cl6, cl7)
+        self.assertLessEqual(cl6, cl7)
+        self.assertNotEqual(cl6, cl7)
+        self.assertGreater(cl7, cl6)
+        self.assertGreaterEqual(cl7, cl6)
+        self.assertNotEqual(cl7, cl6)
 
     def test_str(self):
         cl1 = CustomList([1, 2, 3, 4, 5])
